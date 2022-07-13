ab130ab1524d09bd41b69d300810fab8b52003d4
Arabov.Mullosharaf <Arabov.Mullosharaf@DESKTOP-1K0QUP5>
2022-07-13 16:13:18 +0300

    Add project files.

---
+++ DevOpsDemo/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using DevOpsDemo.Models;
+using DevOpsDemo.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DevOpsDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        IPostRepository postRepository;
+
+        public HomeController(IPostRepository _postRepository)
+        {
+           // _logger = logger;
+            postRepository=_postRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var model = postRepository.GetPosts();
+            return View(model);
+
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
